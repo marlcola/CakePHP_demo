c1da9b34ebcbe380d349230f0390be4c64a95dd7
mori <mori@mori-PC>
2013-09-08 23:45:12 +0900

    I edited a configuration file for the database.

---
+++ app/Config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'masaki',
+		'password' => 'cpd0909',
+		'database' => 'cakephp_prac_db',
+		'prefix' => '',
+		'encoding' => 'utf8'
+	);
+}

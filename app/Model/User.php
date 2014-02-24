53196f5c4672e8d95d719f1cbc63381872acb4ec
mori <mori@mori-PC>
2014-02-25 00:29:59 +0900

    User モデルの作成
    	モデルプロパティ、バリデーションなどは後で記述する

---
+++ app/Model/User.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of User
+ *
+ * @author mori
+ */
+class User extends AppModel{
+    
+}
+
+?>

eb3bfc2a17185282505214334fb7ac6b0ded4cac
mori <mori@mori-PC>
2014-02-25 00:47:43 +0900

    Users コントローラを作成
    	scaffold を利用して、モデルをテスト
    
    　プライマリキーの設定に不備があり、エラーが発生。
    
    　User モデルの primaryKey 属性を設定してみる。

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UsersController
+ *
+ * @author mori
+ */
+class UsersController extends AppController{
+    public $scaffold;
+}
+
+?>
